e6b409abc1c0659eccfae4b70d8a3a2e63f5399c
Walber Vaz <wvs.walber@gmail.com>
2024-10-04 21:33:55 -0300

    refactor: Update useFetch hook and App component
    - Refactor the useFetch hook to include error handling and retry functionality
    - Update the App component to handle error display and retry logic
    - Improve the UI with better loading and error messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,107 @@
-import { useFetch } from "@/hooks/useFetch"
-import { useState } from "react"
+import { useEffect, useRef, useState } from 'react'
+
+import { type FetchError, useFetch } from '@/hooks/useFetch'
+
+interface Post {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
 
 export const App = () => {
     const [page, setPage] = useState(1)
-    const { data, loading, error } = useFetch<
-        Array<{
-            userId: number
-            id: number
-            title: string
-            body: string
-        }>
-    >(`https://jsonplaceholder.typicode.com/posts?page=${page}`)
+    const { data, loading, error, retry, attemptsLeft } = useFetch<Post[], FetchError>(
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`,
+        // 'https://httpbin.org/status/500',
+    )
+    const dialogRef = useRef<HTMLDialogElement | null>(null)
 
-    if (loading) {
-        return <p>Loading...</p>
+    useEffect(() => {
+        if (error) {
+            dialogRef.current?.showModal()
+        } else {
+            dialogRef.current?.close()
+        }
+    }, [error])
+
+    const handlePageChange = (newPage: number) => {
+        if (newPage < 1 && !error) {
+            dialogRef.current?.showModal()
+            return
+        }
+
+        setPage(newPage)
     }
 
-    if (error) {
-        return <p>Error: {error.message}</p>
+    const handleCloseDialog = () => {
+        dialogRef.current?.close()
+    }
+
+    const handleRetry = () => {
+        retry()
+    }
+
+    if (loading) {
+        return (
+            <div className="flex h-screen items-center justify-center">
+                <p className="text-lg font-semibold text-gray-600">Loading...</p>
+            </div>
+        )
     }
 
     return (
         <main className="container mx-auto">
-            <h1 className="text-center text-4xl">Template w2k</h1>
-            <ul>
-                {data?.map((post: any) => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
-            <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => setPage(page + 1)}
-            >
-                Load more
-            </button>
+            <h1 className="mb-4 text-center text-4xl font-bold">Template w2k</h1>
+            {data && (
+                <ul className="list-disc pl-5">
+                    {data.map((post: Post) => (
+                        <li key={post.id} className="mb-2">
+                            <h2 className="text-lg font-bold">{post.title}</h2>
+                            <p className="text-gray-600">{post.body}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+
+            <div className="mt-6 flex justify-center space-x-4">
+                <button
+                    className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                    onClick={() => handlePageChange(page - 1)}
+                >
+                    Previous page
+                </button>
+                <button
+                    className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                    onClick={() => handlePageChange(page + 1)}
+                >
+                    Next page
+                </button>
+            </div>
+
+            <dialog ref={dialogRef} className="rounded-lg border-2 border-gray-400 p-4">
+                {error ? (
+                    <>
+                        <p className="text-red-500">Error: {error.message}</p>
+                        <button
+                            className={`mt-4 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700 ${attemptsLeft === 0 ? 'cursor-not-allowed' : 'cursor-pointer'} `}
+                            onClick={handleRetry}
+                        >
+                            Retry ({attemptsLeft} attempts left)
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <p>Page number must be greater than 0</p>
+                        <button
+                            className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                            onClick={handleCloseDialog}
+                        >
+                            OK
+                        </button>
+                    </>
+                )}
+            </dialog>
         </main>
     )
 }
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,51 +1,81 @@
-import { useState, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
-export function useFetch<T>(url: string): {
-    data: T | null
+export interface FetchError {
+    message: string
+}
+
+interface RetryProps {
+    maxRetries?: number
+}
+
+export function useFetch<dataType, errorType = FetchError>(
+    url: string,
+): {
+    data: dataType | null
     loading: boolean
-    error: any
+    error: errorType | null
+    retry: (props?: RetryProps) => void
+    attemptsLeft: number
 } {
-    const [data, setData] = useState<T | null>(null)
+    const [data, setData] = useState<dataType | null>(null)
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useState<{ message: string } | null>(null)
+    const [error, setError] = useState<errorType | null>(null)
+    const controller = useRef<AbortController | null>(null)
+    const [retryCount, setRetryCount] = useState(0)
+    const [maxRetries, setMaxRetries] = useState(0)
 
-    const abortControllerRef = useRef<AbortController | null>(null)
+    const fetchData = useCallback(async () => {
+        controller.current?.abort()
+        controller.current = new AbortController()
 
-    const fetchData = async () => {
         try {
-            abortControllerRef.current = new AbortController()
+            setLoading(true)
+            setError(null)
             const response = await fetch(url, {
-                signal: abortControllerRef.current.signal,
+                signal: controller.current.signal,
             })
 
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`)
             }
 
-            const result = (await response.json()) as T
+            const result = (await response.json()) as dataType
             setData(result)
-        } catch (error: any) {
-            if (error.name === 'AbortError') {
-                console.error('Request aborted')
-                return
+        } catch (error: unknown) {
+            if ((error as Error).name !== 'AbortError') {
+                setError({
+                    message: (error as Error).message || 'An unknown error occurred',
+                } as errorType)
             }
-            setError({
-                message: error?.message || 'An unknown error occurred',
-            })
         } finally {
             setLoading(false)
         }
-    }
+    }, [url])
 
     useEffect(() => {
         fetchData()
 
         return () => {
-            if (abortControllerRef.current) {
-                abortControllerRef.current.abort()
-            }
+            controller.current?.abort()
         }
-    }, [url])
+    }, [url, retryCount, fetchData])
 
-    return { data, loading, error }
+    const retry = ({ maxRetries = 3 }: RetryProps = {}) => {
+        if (retryCount < maxRetries) {
+            setMaxRetries(maxRetries)
+            setTimeout(() => {
+                setRetryCount((prev) => prev + 1)
+            }, 1000)
+        } else {
+            console.warn('Máximo de tentativas atingido.')
+        }
+    }
+
+    return {
+        data,
+        loading,
+        error,
+        retry,
+        attemptsLeft: maxRetries - retryCount,
+    }
 }
